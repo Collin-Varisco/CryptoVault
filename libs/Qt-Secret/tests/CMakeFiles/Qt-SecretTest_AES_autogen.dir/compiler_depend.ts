feb305e7bd110a64bfb242aff6b761f3911782a1
Collin-Varisco-UL <c00304484@louisiana.edu>
2021-10-17 15:22:13 -0700

    Remove main branch files for future merge.

--- libs/Qt-Secret/tests/CMakeFiles/Qt-SecretTest_AES_autogen.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for Qt-SecretTest_AES_autogen.
--- libs/Qt-Secret/tests/CMakeFiles/testQt-SecretRSA.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/testQt-SecretRSA"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/testQt-SecretRSA.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
