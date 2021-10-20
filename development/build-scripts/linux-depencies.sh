962296d4f9a66805887397e68b5627dc3a4f6433
Collin Varisco <c00304484@louisiana.edu>
2021-10-20 18:31:04 +0000

    script no longer clones repository and builds it. It only installs dependencies for whatever distribution it runs on.

--- development/build-scripts/linux-depencies.sh
+++ development/build-scripts/linux-depencies.sh
@@ -6,9 +6,3 @@ if [ -f /etc/lsb-release ]; then
 	yes | sudo apt-get install qt5-default
     yes | sudo apt-get install build-essential
 fi
-git clone https://gitlab.com/collin-varisco-ul/ul-cryptovault.git
-cd ul-cryptovault
-mkdir build
-cd build
-cmake ..
-make
